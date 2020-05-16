9bdd4eb1f1350640467ed70bfe99367728d0a3c1
Alberto <alberto@decabeza.es>
2020-05-16 06:07:41 +0200

    Improve styles for budgets voting phase (#20)
    Improve styles for budgets voting phase

--- spec/system/budgets/investments_spec.rb
+++ spec/system/budgets/investments_spec.rb
@@ -1046,12 +1046,12 @@ describe "Budget Investments" do
 
       visit budget_investments_path(budget, heading_id: heading.id)
 
-      expect(page).not_to have_link("Check and confirm my ballot")
+      expect(page).not_to have_link("Submit my ballot")
       expect(page).not_to have_css("#progress_bar")
 
       within("#sidebar") do
         expect(page).not_to have_content("My ballot")
-        expect(page).not_to have_link("Check and confirm my ballot")
+        expect(page).not_to have_link("Submit my ballot")
       end
     end
 
@@ -1687,12 +1687,12 @@ describe "Budget Investments" do
 
       visit budget_investments_path(budget, heading_id: heading.id)
 
-      expect(page).to have_link("Check and confirm my ballot")
+      expect(page).to have_link("Submit my ballot")
       expect(page).to have_css("#progress_bar")
 
       within("#sidebar") do
         expect(page).to have_content("My ballot")
-        expect(page).to have_link("Check and confirm my ballot")
+        expect(page).to have_link("Submit my ballot")
       end
     end
 
