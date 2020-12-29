7e31a3bdcb2c3cab2681efbccc8157cfd43bddfb
taitus <sebastia.roig@gmail.com>
2020-12-29 12:14:42 +0100

    Allow update the status of notifications
    You can update the same "notifications" section that we allow you to update in "my
     account".
    This "unsubscribe" section differs from the "my account" section because we do not
    need to be logged in to update the status of the notifications.

--- app/controllers/unsubscribes_controller.rb
+++ app/controllers/unsubscribes_controller.rb
@@ -5,9 +5,19 @@ class UnsubscribesController < ApplicationController
   def edit
   end
 
+  def update
+    @user.update(unsubscribe_params)
+    redirect_to edit_unsubscribe_path, notice: t("flash.actions.save_changes.notice")
+  end
+
   private
 
     def set_user
       @user = User.find_by_unsubscribe_hash(params[:unsubscribe_hash])
+    end
+
+    def unsubscribe_params
+      attributes = [:email_on_comment, :email_on_comment_reply, :email_on_direct_message, :email_digest, :newsletter]
+      params.require(:user).permit(*attributes)
     end
 end
--- config/routes/unsubscribe.rb
+++ config/routes/unsubscribe.rb
@@ -1,1 +1,2 @@
 get "edit_unsubscribe/:unsubscribe_hash", to: "unsubscribes#edit",  as: "edit_unsubscribe"
+patch "unsubscribe/:unsubscribe_hash", to: "unsubscribes#update", as: "unsubscribe"
