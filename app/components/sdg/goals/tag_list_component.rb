4aba70a81b3e97070e21042eee5f18f35fba0c59
Javi Martín <javim@elretirao.net>
2021-01-21 19:06:01 +0100

    Add list of targets to SDG tag list
    Co-authored-by: Senén Rodero Rodríguez <senenrodero@gmail.com>

--- app/components/sdg/goals/tag_list_component.rb
+++ app/components/sdg/goals/tag_list_component.rb
@@ -13,50 +13,81 @@ class SDG::Goals::TagListComponent < ApplicationComponent
 
   private
 
-    def links
-      [*goal_links, see_more_link]
+    def record
+      record_or_name if record_or_name.respond_to?(:sdg_goals)
+    end
+
+    def goal_links_with_see_more
+      [*goal_links, see_more_link(goals, "goals")]
     end
 
     def goal_links
-      goals.limit(limit).map do |goal|
+      goals.order(:code).limit(limit).map do |goal|
         [
           render(SDG::Goals::IconComponent.new(goal)),
           index_by_goal(goal),
-          title: filter_text(goal)
+          title: filter_text(goal, "goals")
         ]
       end
     end
 
     def goals
-      if record_or_name.respond_to?(:sdg_goals)
-        record_or_name.sdg_goals.order(:code)
-      else
-        SDG::Goal.order(:code)
+      record&.sdg_goals || SDG::Goal.all
+    end
+
+    def target_links_with_see_more
+      [*target_links, see_more_link(targets, "targets")]
+    end
+
+    def target_links
+      targets.sort[0..(limit.to_i - 1)].map do |target|
+        [
+          "#{SDG::Target.model_name.human} #{target.code}",
+          index_by_target(target),
+          title: filter_text(target, "targets"),
+          data: { code: target.code }
+        ]
       end
     end
 
-    def see_more_link
-      return unless limit && count_out_of_limit > 0
+    def targets
+      record&.sdg_targets || []
+    end
 
-      [
-        "#{count_out_of_limit}+",
-        polymorphic_path(record_or_name),
-        class: "more-goals", title: t("sdg.goals.filter.more", count: count_out_of_limit)
-      ]
+    def see_more_link(collection, namespace)
+      count = count_out_of_limit(collection)
+
+      if count > 0
+        [
+          "#{count}+",
+          polymorphic_path(record_or_name),
+          class: "more-#{namespace}", title: t("sdg.#{namespace}.filter.more", count: count)
+        ]
+      end
     end
 
     def index_by_goal(goal)
-      polymorphic_path(model, advanced_search: { goal: goal.code })
+      index_by(goal: goal.code)
     end
 
-    def filter_text(goal)
-      t("sdg.goals.filter.link",
+    def index_by_target(target)
+      index_by(target: target.code)
+    end
+
+    def index_by(advanced_search)
+      polymorphic_path(model, advanced_search: advanced_search)
+    end
+
+    def filter_text(goal_or_target, namespace)
+      t("sdg.#{namespace}.filter.link",
         resources: model.model_name.human(count: :other),
-        code: goal.code)
+        code: goal_or_target.code)
     end
 
-    def count_out_of_limit
-      goals.size - limit
+    def count_out_of_limit(collection)
+      return 0 unless limit
+
+      collection.size - limit
     end
 
     def model
--- spec/components/sdg/goals/tag_list_component_spec.rb
+++ spec/components/sdg/goals/tag_list_component_spec.rb
@@ -1,7 +1,12 @@
 require "rails_helper"
 
 describe SDG::Goals::TagListComponent, type: :component do
-  let(:debate) { create(:debate, sdg_goals: [SDG::Goal[1], SDG::Goal[3]]) }
+  let(:debate) do
+    create(:debate,
+           sdg_goals: [SDG::Goal[1], SDG::Goal[3]],
+           sdg_targets: [SDG::Target[1.1], SDG::Target[3.2], create(:sdg_local_target, code: "3.2.1")]
+          )
+  end
   let(:component) { SDG::Goals::TagListComponent.new(debate) }
 
   before do
@@ -25,51 +30,109 @@ describe SDG::Goals::TagListComponent, type: :component do
     expect(page).not_to have_css "li"
   end
 
-  it "renders a list of goals" do
-    render_inline component
+  describe "goal list" do
+    let(:goal_list) { page.find(".sdg-goal-tag-list") }
 
-    expect(page).to have_css "li", count: 2
-  end
-
-  it "renders links for each goal" do
-    render_inline component
-
-    expect(page).to have_selector "a", count: 2
-    expect(page).to have_link "1. No Poverty",
-                              title: "See all Debates related to goal 1",
-                              href: "/debates?advanced_search#{CGI.escape("[goal]")}=1"
-    expect(page).to have_link "3. Good Health and Well-Being",
-                              title: "See all Debates related to goal 3",
-                              href: "/debates?advanced_search#{CGI.escape("[goal]")}=3"
-  end
-
-  it "orders goals by code" do
-    render_inline component
-
-    expect(page.first("a")[:title]).to end_with "goal 1"
-  end
-
-  it "renders a link for more goals when out of limit" do
-    component = SDG::Goals::TagListComponent.new(debate, limit: 1)
-
-    render_inline component
-
-    expect(page).to have_selector "a", count: 2
-    expect(page).to have_link "1. No Poverty"
-    expect(page).to have_link "1+",
-                              title: "One more goal",
-                              href: "/debates/#{debate.to_param}"
-  end
-
-  context "given a class name" do
-    let(:component) { SDG::Goals::TagListComponent.new("Debate") }
-
-    it "renders all goals ordered by code" do
+    it "renders a list of goals" do
       render_inline component
 
-      expect(page).to have_selector "li", count: 17
-      expect(page.first("a")[:title]).to end_with "goal 1"
-      expect(page.all("a").last[:title]).to end_with "goal 17"
+      expect(goal_list).to have_css "li", count: 2
+    end
+
+    it "renders links for each goal" do
+      render_inline component
+
+      expect(goal_list).to have_selector "a", count: 2
+      expect(goal_list).to have_link "1. No Poverty",
+                                title: "See all Debates related to goal 1",
+                                href: "/debates?advanced_search#{CGI.escape("[goal]")}=1"
+      expect(goal_list).to have_link "3. Good Health and Well-Being",
+                                title: "See all Debates related to goal 3",
+                                href: "/debates?advanced_search#{CGI.escape("[goal]")}=3"
+    end
+
+    it "orders goals by code" do
+      render_inline component
+
+      expect(goal_list.first("a")[:title]).to end_with "goal 1"
+    end
+
+    it "renders a link for more goals when out of limit" do
+      component = SDG::Goals::TagListComponent.new(debate, limit: 1)
+
+      render_inline component
+
+      expect(goal_list).to have_selector "a", count: 2
+      expect(goal_list).to have_link "1. No Poverty"
+      expect(goal_list).to have_link "1+",
+                                title: "One more goal",
+                                href: "/debates/#{debate.to_param}"
+    end
+
+    context "given a class name" do
+      let(:component) { SDG::Goals::TagListComponent.new("Debate") }
+
+      it "renders all goals ordered by code" do
+        render_inline component
+
+        expect(goal_list).to have_selector "li", count: 17
+        expect(goal_list.first("a")[:title]).to end_with "goal 1"
+        expect(goal_list.all("a").last[:title]).to end_with "goal 17"
+      end
+    end
+  end
+
+  describe "target list" do
+    let(:target_list) { page.find(".sdg-target-tag-list") }
+
+    it "renders a list of targets" do
+      render_inline component
+
+      expect(target_list).to have_css "li", count: 3
+    end
+
+    it "renders links for each target" do
+      render_inline component
+
+      expect(target_list).to have_css "li", count: 3
+      expect(target_list).to have_link "target 1.1",
+                                       title: "See all Debates related to target 1.1",
+                                       href: "/debates?advanced_search#{CGI.escape("[target]")}=1.1"
+      expect(target_list).to have_link "target 3.2",
+                                       title: "See all Debates related to target 3.2",
+                                       href: "/debates?advanced_search#{CGI.escape("[target]")}=3.2"
+
+      expect(target_list).to have_link "target 3.2.1",
+                                       title: "See all Debates related to target 3.2.1",
+                                       href: "/debates?advanced_search#{CGI.escape("[target]")}=3.2.1"
+    end
+
+    it "orders targets by code" do
+      render_inline component
+
+      expect(target_list.first("a")[:title]).to end_with "target 1.1"
+    end
+
+    it "renders a link for more targets when out of limit" do
+      component = SDG::Goals::TagListComponent.new(debate, limit: 1)
+
+      render_inline component
+
+      expect(target_list).to have_selector "a", count: 2
+      expect(target_list).to have_link "target 1.1"
+      expect(target_list).to have_link "2+",
+                                       title: "2 more targets",
+                                       href: "/debates/#{debate.to_param}"
+    end
+
+    context "given a class name" do
+      let(:component) { SDG::Goals::TagListComponent.new("Debate") }
+
+      it "renders nothing" do
+        render_inline component
+
+        expect(page).not_to have_selector(".sdg-target-tag-list")
+      end
     end
   end
 end
