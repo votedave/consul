ebe453798c490b907e17b9676bd4cad804cfaf12
Javi Martín <javim@elretirao.net>
2021-01-23 02:09:59 +0100

    Add long description to goals

---
+++ spec/components/sdg/goals/show_component_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe SDG::Goals::ShowComponent, type: :component do
+  let!(:goal_1) { SDG::Goal[1] }
+
+  before do
+    Setting["feature.sdg"] = true
+    allow(SDG::Widget::Feed).to receive(:for_goal).and_return([])
+    allow(Widget::Feeds::ParticipationComponent).to receive(:new).and_return([])
+  end
+
+  it "renders a heading" do
+    component = SDG::Goals::ShowComponent.new(goal_1)
+
+    render_inline component
+
+    expect(page).to have_css ".goal-title"
+    expect(page).to have_content "No Poverty"
+  end
+
+  it "renders a long description" do
+    component = SDG::Goals::ShowComponent.new(goal_1)
+
+    render_inline component
+
+    expect(page).to have_css "#description_goal_#{goal_1.code}"
+    expect(page).to have_content "Globally, the number of people living in extreme poverty declined from 36 per cent in 1990 to 10 "
+  end
+end
